6e6f4869b43db4aa270709acc3d699f3aead3030
Mike La <4721509+mictr@users.noreply.github.com>
2025-02-19 22:03:53 -0800

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  // replace all non-characters with empty string
+  const sanitizedInput = input.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+  console.log({ sanitizedInput });
+  // check for palindrome
 
+  for (
+    let startIndex = 0, endIndex = sanitizedInput.length - 1;
+    startIndex < endIndex;
+    ++startIndex, --endIndex
+  ) {
+    if (sanitizedInput[startIndex] !== sanitizedInput[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
